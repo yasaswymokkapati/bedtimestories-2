85bb92b37bf6a0246b642d0acfebb8f8e64c39b8
yasaswymokkapati <sudhaa.poornima@gmail.com>
2020-10-02 12:07:22 +0530

    hey!

--- screens/ReadStoryScreen.js
+++ screens/ReadStoryScreen.js
@@ -1,11 +1,41 @@
 import * as React from 'react';
+import db from '../config';
+import firebase from 'firebase';
+import {Header} from 'react-native-elements';
 import {Text, View, StyleSheet} from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
 
 export default class ReadStory extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            allStories : []
+        }
+    }
+    
+componentDidMount = async ()=>{
+    const query = await db.collection(Stories).get()
+    query.docs.map((doc)=>{
+        this.setState({
+            allTransactions : [...this.state.allTransactions.doc.data()]
+        })
+    })
+}
     render(){
         return(
             <View>
-                <Text>Read Story</Text>
+               <Header title = {"Read Story"} backgroundColor = 'yellow'/> 
+                <FlatList 
+                  data = {this.state.allStories}
+                  renderItem = {({item})=>(
+                      <View style = {{borderBottomWidth : 2}}>
+                          <Text>{"Story Title : " + item.title}</Text>
+                          <Text>{"Story Author : " + item.author}</Text>
+                          <Text>{"Date : " + item.date.toDate()}</Text>
+                          <Text>{"Story : " + item.story}</Text>
+                      </View>
+                  )}
+                  keyExtractor = {(item, index)=> index.toString()}/>
             </View>
         )
     }
